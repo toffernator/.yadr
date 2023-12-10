3da20792130324f842780ae750943cf1f08263c9
Christoffer Gram <christofferkgram@gmail.com>
2023-12-10 23:36:38 +0100

    Delete Unused Modules

--- modules/programs/default.nix
+++
@@ -1,3 +0,0 @@
-[
-  ./rstudio.nix
-]
--- modules/programs/rstudio.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, unstable, vars, ... }:
-
-{
-  config = lib.mkIf (config.rstudio.enable) {
-    environment.systemPackages = with unstable; [
-      rstudio
-    ];
-  };
-}
