ecb08f5cbc44fdc6ebbde4b9e71f07c6e93e67cd
Christoffer Gram <christofferkgram@gmail.com>
2023-12-17 00:32:59 +0100

    Set oxo-carbon as the default colorscheme

--- dotfiles/nvim/.config/nvim/lua/plugins/colors.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,13 +1,13 @@
-return { 
-    -- gruvbox.nvim is the main colorscheme, therefor it has priority 1000 and
-    -- lazy load is false
-    "ellisonleao/gruvbox.nvim", 
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd([[colorscheme gruvbox]])
-        
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
+return {
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme oxocarbon]])
+        end,
+    },
+    { "ellisonleao/gruvbox.nvim" },
+    { "bluz71/vim-moonfly-colors" },
+    { 'kepano/flexoki-neovim', name = 'flexoki' }
 }
