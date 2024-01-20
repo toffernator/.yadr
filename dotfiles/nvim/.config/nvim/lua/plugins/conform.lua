76582f3a56ad521b445f2dcd975f9ad87fab4b7d
Christoffer Gram <christofferkgram@gmail.com>
2024-01-20 18:54:13 +0100

    Update Lazy Packages + Prettier on More JS Related Filetypes

--- dotfiles/nvim/.config/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,37 +1,41 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
     },
-  },
-  -- Everything in opts will be passed to setup()
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      nix = { "nixfmt" }
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            javascript = { { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            ["javascript.tsx"] = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            ["typescript.tsx"] = { { "prettierd", "prettier" } },
+            nix = { "nixfmt" }
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+        -- Customize formatters
+        formatters = {
+            -- Intentionally empty
+        },
     },
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    -- Customize formatters
-    formatters = {
-      -- Intentionally empty
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
