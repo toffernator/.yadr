3033ec0ac5f9299b030f949f622485958fe0b982
Christoffer Gram <74768013+toffernator@users.noreply.github.com>
2024-04-13 00:23:01 +0200

    feat!: Move SSH Server to Pi config + Add Supposed Wifi Fix

--- nixos/hosts/raspberry-pi/configuration.nix
+++ nixos/hosts/raspberry-pi/configuration.nix
@@ -14,4 +14,37 @@
   # !!! Adding a swap file is optional, but recommended if you use RAM-intensive applications that might OOM otherwise. 
   # Size is in MiB, set to whatever you want (though note a larger value will use more disk space).
   # swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
+  };
+
+  # Apparently WiFi is broken on Raspberry Pi 3B+, I have a 3B but leaving this
+  # here just in case
+  systemd.services.iwd.serviceConfig.Restart = "always";
+  hardware = {
+    enableRedistributableFirmware = true;
+    firmware = [ pkgs.wireless-regdb ];
+  };
+  networking = {
+    useDHCP = false;
+    interfaces.wlan0.useDHCP = true;
+    networkmanager.wifi.backend = "iwd";
+    wireless.iwd.enable = true;
+  };
+  boot = {
+    extraModprobeConfig = ''
+      options cfg80211 ieee80211_regdom="US"
+    '';
+  };
+
+  system = { stateVersion = "23.11"; };
 }
