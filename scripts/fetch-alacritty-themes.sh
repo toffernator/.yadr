249f3420225f5840755690f2f5c36b855f13e6ee
Christoffer Gram <74768013+toffernator@users.noreply.github.com>
2024-04-15 21:34:19 +0200

    feat: Shallow Copy in fetch-alacritty-themes.sh

--- scripts/fetch-alacritty-themes.sh
+++ scripts/fetch-alacritty-themes.sh
@@ -8,7 +8,7 @@ set -o nounset
 rm --recursive --force ../dotfiles/alacritty/themes
 
 mkdir -p ../dotfiles/alacritty/themes
-git clone git@github.com:alacritty/alacritty-theme.git ../dotfiles/alacritty/themes
+git clone --depth 1 git@github.com:alacritty/alacritty-theme.git ../dotfiles/alacritty/themes
 rm --recursive --force ../dotfiles/alacritty/themes/.git
 
 git add ../dotfiles/alacritty/themes
