ac918277eb56de3d2421520a94ae3dc754df45cc
Christoffer Gram <74768013+toffernator@users.noreply.github.com>
2024-04-15 21:40:43 +0200

    feat: fetch-alacritty-themes.sh Leaves Only the Theme Files

--- scripts/fetch-alacritty-themes.sh
+++ scripts/fetch-alacritty-themes.sh
@@ -9,7 +9,13 @@ rm --recursive --force ../dotfiles/alacritty/themes
 
 mkdir -p ../dotfiles/alacritty/themes
 git clone --depth 1 git@github.com:alacritty/alacritty-theme.git ../dotfiles/alacritty/themes
-rm --recursive --force ../dotfiles/alacritty/themes/.git
+
+rm --recursive --force ../dotfiles/alacritty/themes/.git images
+rm --force ../dotfiles/alacritty/themes/README.md print_colors.sh
+
+mv ../dotfiles/alacritty/themes/themes/* ../dotfiles/alacritty/themes
+rm --recursive ../dotfiles/alacritty/themes/themes
 
 git add ../dotfiles/alacritty/themes
 git commit --message "chore: Run scripts/fetch-alacritty-themes.sh"
+
