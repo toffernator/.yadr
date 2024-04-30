757150df04ff098893f0f3a9afa1301020c1b952
toffernator <74768013+toffernator@users.noreply.github.com>
2024-04-30 20:27:27 +0200

    feat: Add More fugitive Keybinds

--- dotfiles/nvim/lua/plugins/fugitive.lua
+++ dotfiles/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,12 @@
 return {
     "tpope/vim-fugitive",
     keys = {
-        { "<leader>gg", "<cmd>Git<cr>", desc="Git" },
+        { "<leader>gg",  "<cmd>Git<cr>",          desc = "Git" },
+        { "<leader>gap", "<cmd>Git add -p .<cr>", desc = "Git" },
+        { "<leader>gci", "<cmd>Git commit<cr>",   desc = "Git" },
+        { "<leader>gpu", "<cmd>Git push<cr>",     desc = "Git" },
+        { "<leader>gpl", "<cmd>Git pull<cr>",     desc = "Git" },
+        { "<leader>gf",  "<cmd>Git commit<cr>",   desc = "Git" },
     },
-    cmd = {"Git", "G"}
+    cmd = { "Git", "G" }
 }
-
