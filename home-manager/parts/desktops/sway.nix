8498e864bd377b6377e56493338e3b63e5df1f80
toffernator <74768013+toffernator@users.noreply.github.com>
2024-05-07 16:45:58 +0200

    feat: Copy Sway Configuration from NixOs Wiki

--- home-manager/parts/desktops/sway.nix
+++ home-manager/parts/desktops/sway.nix
@@ -14,7 +14,18 @@ in with lib; {
 
   };
   config = mkIf (config.sway.enable) {
-    wayland.windowManager.sway = { enable = true; };
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        modifier = "Mod4";
+        # Use kitty as default terminal
+        terminal = "alacritty";
+        startup = [
+          # Launch Firefox on start
+          { command = "firefox"; }
+        ];
+      };
+    };
   };
 }
 
