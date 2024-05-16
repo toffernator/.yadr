3ba00d0bdbd6d335ebd9a6d8506e1b923be0c190
toffernator <74768013+toffernator@users.noreply.github.com>
2024-05-16 23:54:46 +0200

    feat(nvim): Add trouble.nvim + Fix that Telescope Disabled Font Icons

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-ui-select.nvim' },
-        { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+        { 'nvim-tree/nvim-web-devicons' },
         { "tsakirist/telescope-lazy.nvim" },
         { "Snikimonkd/telescope-git-conflicts.nvim" },
     },
