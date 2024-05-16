06238d282762f2453f208984fe1dddb8bab2b11d
toffernator <74768013+toffernator@users.noreply.github.com>
2024-05-16 23:17:43 +0200

    feat(nvim): More Git Functionality with Gitsigns & Telescope

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -45,14 +45,17 @@ return {
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[F]iles' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'by [G]rep' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[G]rep' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[R]esume' })
-        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[.]Recent Files' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-        vim.keymap.set('n', '<leader>gc', "<cmd>Telescope conflicts<Enter>", { desc = '[C]onflicts' })
+        vim.keymap.set('n', '<leader>gC', "<cmd>Telescope conflicts<Enter>", { desc = '[C]onflicts' })
+        vim.keymap.set('n', '<leader>gb', "<cmd>Telescope git_branches<Enter>", { desc = '[B]ranches' })
+        vim.keymap.set('n', '<leader>gh', "<cmd>Telescope git_commits<Enter>", { desc = '[h]istory' })
+        vim.keymap.set('n', '<leader>gH', "<cmd>Telescope git_bcommits<Enter>", { desc = '[H]istory buffer' })
 
         vim.keymap.set('n', '<leader>ss', function()
             builtin.builtin { include_extensions = true }
--- dotfiles/nvim/lua/plugins/whichkey.lua
+++ dotfiles/nvim/lua/plugins/whichkey.lua
@@ -7,23 +7,17 @@ return {
             vim.o.timeout = true
             vim.o.timeoutlen = 300
         end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        config = function() -- This is the function that runs, AFTER loading
+        config = function()
             local wk = require("which-key")
             require('which-key').setup()
 
-            -- Document existing key chains
             require('which-key').register({
                 c = { name = '[C]ode', _ = 'which_key_ignore' },
                 d = { name = '[D]ocument', _ = 'which_key_ignore' },
                 r = { name = '[R]ename', _ = 'which_key_ignore' },
                 s = { name = '[S]earch', _ = 'which_key_ignore' },
                 w = { name = '[W]orkspace', _ = 'which_key_ignore' },
-                h = { name = '[H]arpoon', _ = 'which_key_ignore' },
+                h = { name = '[H]arpoon / [H]unk', _ = 'which_key_ignore' },
                 g = { name = '[G]it', _ = 'which_key_ignore' },
             }, { prefix = "<leader>" })
             local ignore_keys = { "d", "y", "Y", "k", "j", "e", "x", "q", "/", "<leader>" }
