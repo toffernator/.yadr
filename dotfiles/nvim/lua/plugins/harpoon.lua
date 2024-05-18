8d3a58a1566f8a6c76efed06131b1bf5d8ec3fea
toffernator <74768013+toffernator@users.noreply.github.com>
2024-05-18 12:09:06 +0200

    fix(nvim): Harpoon2 Config WAS ALL KINDS OF WRONG Ok

--- dotfiles/nvim/lua/plugins/harpoon.lua
+++ dotfiles/nvim/lua/plugins/harpoon.lua
@@ -6,14 +6,10 @@ return {
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[A]dd" })
         vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
             { desc = "[E]xplore" })
-        vim.keymap.set("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-            { desc = "File [1]" })
-        vim.keymap.set("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-            { desc = "File [2]" })
-        vim.keymap.set("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-            { desc = "File [3]" })
-        vim.keymap.set("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
-            { desc = "File [4]" })
+        vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "File [1]" })
+        vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "File [2]" })
+        vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "File [3]" })
+        vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "File [4]" })
     end,
     dependencies = { 'nvim-lua/plenary.nvim' },
 }
