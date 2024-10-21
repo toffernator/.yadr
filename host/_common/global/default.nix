a14d2be9379a82000257fe70e0312826153e27cf
toffernator <74768013+toffernator@users.noreply.github.com>
2024-10-21 17:28:26 +0200

    feat: Add upower.nix and zsh.nix

--- host/_common/global/default.nix
+++ host/_common/global/default.nix
@@ -1,3 +1,11 @@
 { ... }: {
-  imports = [ ./console.nix ./i18n.nix ./nix.nix ./nixpkgs.nix ./security.nix ];
+  imports = [
+    ./console.nix
+    ./i18n.nix
+    ./nix.nix
+    ./nixpkgs.nix
+    ./security.nix
+    ./upower.nix
+    ./zsh.nix
+  ];
 }
