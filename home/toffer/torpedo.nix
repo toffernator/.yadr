119c3b063f8a9070e87cbf70fbb9cf2b5b76c886
toffernator <74768013+toffernator@users.noreply.github.com>
2024-10-22 22:14:26 +0200

    feat: Add firefox

--- home/toffer/torpedo.nix
+++ home/toffer/torpedo.nix
@@ -1,15 +1,18 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ../_part/cli/git
     ../_part/cli/bat.nix
     ../_part/cli/zoxide.nix
-    
-   ../_part/editor/nvim
+
+    ../_part/editor/nvim
 
     ../_part/desktop/sway
   ];
 
   neovim.enable = true;
 
-  home = { stateVersion = "24.05"; };
+  home = {
+    packages = with pkgs; [ firefox ];
+    stateVersion = "24.05";
+  };
 }
