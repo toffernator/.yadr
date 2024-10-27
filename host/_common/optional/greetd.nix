cb450968597892c35df7c9ade64302b0036ef67b
toffernator <74768013+toffernator@users.noreply.github.com>
2024-10-27 10:17:07 +0100

    feat: enable greetd with tuigreet

--- host/_common/optional/greetd.nix
+++ host/_common/optional/greetd.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
+  environment = { systemPackages = with pkgs; [ greetd.tuigreet ]; };
+
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+        command = "${lib.getExe pkgs.greetd.tuigreet} --time --remember --cmd ${
+            lib.getExe pkgs.sway
+          }";
+        # command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
       };
     };
   };
